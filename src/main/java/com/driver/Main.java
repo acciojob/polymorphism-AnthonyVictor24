c375938a55a45f9003665cde06d2f9b00e73ad5e
AnthonyVictor24 <arogyaanthonyvictor24@gmail.com>
2024-01-15 12:38:52 +0530

    Polymorphism done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public int product(int x, int y) {
+            return 0;
+        }
+        public int product(int x, int y, int z) {
+            return 0;
+        }
+        public double product(double x, double y) {
+            return 0.0;
+        }
+    }
+    public static void main(String[] args) {
+//create object of Product in Main function called p
+        Product p = new Product();
+        p.product(5,6);
+        p.product(1,3,4);
+        p.product(2.0,3.0);
+    }
 }
